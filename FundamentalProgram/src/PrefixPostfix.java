e6333d41511284fb9598082abe7b33b4d7c987d1
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 00:13:01 +0530

    first commit

---
+++ FundamentalProgram/src/PrefixPostfix.java
@@ -0,0 +1,17 @@
+
+public class PrefixPostfix {
+
+	public static void main(String[] args) {
+		int numA=5;
+		int numB=10;
+		int numC=0;
+		
+		numC=--numC+numB--;
+		System.out.println(numA);
+		System.out.println(numC);
+		System.out.println(numB);
+		
+
+	}
+
+}
