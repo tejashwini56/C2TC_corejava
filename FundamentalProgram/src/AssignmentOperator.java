deded84c25388ac7bd7c91f97f098d9ac427c7fd
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 00:22:01 +0530

    first commit

---
+++ FundamentalProgram/src/AssignmentOperator.java
@@ -0,0 +1,21 @@
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		int c=3;
+		
+		a+=5;
+		b*=4;
+		c+=a*b;
+		c %=6;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		
+
+	}
+
+}
