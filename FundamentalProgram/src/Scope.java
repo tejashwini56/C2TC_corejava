6a81e3b1e7d351469ae620b3ef4627815aff9889
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 00:08:20 +0530

    first commit

---
+++ FundamentalProgram/src/Scope.java
@@ -0,0 +1,17 @@
+
+public class Scope {
+
+	public static void main(String[] args) {
+		int outer=1;
+		{
+			int inner=2;
+			System.out.println("inner="+inner);
+			System.out.println("outer="+outer);
+		}
+		int inner=3;
+		System.out.println("inner="+inner);
+		System.out.println("outer="+outer);
+
+	}
+
+}
