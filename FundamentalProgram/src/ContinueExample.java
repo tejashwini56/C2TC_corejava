a9b01ebee94d064b25831f82a91216380e2a1571
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 00:25:26 +0530

    first commit

---
+++ FundamentalProgram/src/ContinueExample.java
@@ -0,0 +1,17 @@
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lim=10;
+		int sum=0;
+		for(int i=1;i<=lim;i++) {
+			if(i%4==0) {
+				continue;
+			}
+			sum+=i;
+		}
+System.out.println(sum);
+	}
+
+}
