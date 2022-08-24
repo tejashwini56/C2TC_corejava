85d4ab183bb186f5b9229b9e8e5ab9aea0103918
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 00:34:03 +0530

    first commit

---
+++ FundamentalProgram/src/DowhileExample.java
@@ -0,0 +1,15 @@
+
+public class DowhileExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do
+		{
+			System.out.println(i+" ");
+			i++;
+		}while(i<10);
+
+	}
+
+}
