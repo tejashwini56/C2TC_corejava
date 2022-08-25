e470f60ed98c8d75f02c90652afdef9898ce75d4
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 10:27:57 +0530

    first commit

---
+++ FundamentalProgram/src/SwitchExample.java
@@ -0,0 +1,18 @@
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		switch(i) {
+		case 0:
+			System.out.println("1 is 0");
+		case 1:
+			System.out.println("1 is 1");
+		case 2:
+			System.out.println("1 is 2");
+		}
+
+	}
+
+}
