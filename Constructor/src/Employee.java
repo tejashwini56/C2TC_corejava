8801d5de2c584d528dda1cef0bb137efc511d561
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 11:48:51 +0530

    first commit

---
+++ Constructor/src/Employee.java
@@ -0,0 +1,23 @@
+
+public class Employee {
+
+
+		int id;
+
+		String name;
+
+		float salary;
+
+		Employee() {
+
+		System.out.println("user defined no-argument "
+
+		+ "constructor executed");
+
+		}
+
+		void display() {
+
+		System.out.println(id + " " + name + " " + salary);
+}
+	}
--- FundamentalProgram/src/RelationalLogicalOprators.java
+++ FundamentalProgram/src/RelationalLogicalOprators.java
@@ -1,15 +1,21 @@
 
-public class RelationalLogicalOprators {
+public class RelationalLogicalOperators {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random rand=new Random();
-		int i=rand.nextInt(100);
-		int j=rand.nextInt(100);
-		
-		System.out.println("i="+i);
-		System.out.println("j="+j);
-;
-	}
-
+public static void main(String[] args) {
+Random rand = new Random();
+int i = rand.nextInt(100);
+int j = rand.nextInt(100);
+System.out.println(&quot;i = &quot; + i);
+System.out.println(&quot;j = &quot; + j);
+System.out.println(&quot;i &gt; j is &quot; + (i &gt; j));
+System.out.println(&quot;i &lt; j is &quot; + (i &lt; j));
+System.out.println(&quot;i &gt;= j is &quot; + (i &gt;= j));
+System.out.println(&quot;i &lt;= j is &quot; + (i &lt;= j));
+System.out.println(&quot;i == j is &quot; + (i == j));
+System.out.println(&quot;i != j is &quot; + (i != j));
+System.out.println(&quot;(i &lt; 10) &amp;&amp; (j &lt; 10) is &quot; + ((i &lt; 10) &amp;&amp;
+(j &lt; 10)));
+System.out.println(&quot;(i &lt; 10) || (j &lt; 10) is &quot; + ((i &lt; 10) ||
+(j &lt; 10)));
+}
 }
