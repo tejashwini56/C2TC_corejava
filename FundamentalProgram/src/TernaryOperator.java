a4e754dd15df4dad74c9ac986840ee7625d9d62f
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 10:52:50 +0530

    first commit

---
+++ FundamentalProgram/src/TernaryOperator.java
@@ -0,0 +1,12 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int v=1;
+        System.out.println(v==1?"A":"B");
+        v++;
+        System.out.println(v==1?"A":"B");
+	}
+
+}
