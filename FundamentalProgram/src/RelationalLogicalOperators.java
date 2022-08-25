14a55ea11beae88d8e8500aec9dc3e601bb0a762
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 11:26:02 +0530

    first commit

---
+++ FundamentalProgram/src/RelationalLogicalOperators.java
@@ -0,0 +1,15 @@
+
+public class RelationalLogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand=new Random();
+		int i=rand.nextInt(100);
+		int j=rand.nextInt(100);
+		
+		System.out.println("i="+i);
+		System.out.println("j="+j);
+;
+	}
+
+}
