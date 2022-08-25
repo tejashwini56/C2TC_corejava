4a90eae2add957ea3e2560cb6dd5b3cf07f96e63
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 11:57:47 +0530

    first commit

---
+++ Constructor/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-gEmployee e1 = new Employee();
+
+		Employee e1 = new Employee();
+
+		Employee e2 = new Employee();
+
+e1.display();
+
+e2.display();
+
+	}
+
+}
