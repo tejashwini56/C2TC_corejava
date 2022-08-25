e2fb9b5dc0db637ff5d7c7f7f932d196a4413630
DELL <DELL@DESKTOP-J9J3KUB>
2022-08-25 12:02:57 +0530

    first commit

---
+++ Constructor/src/ParamertizedConstructor.java
@@ -0,0 +1,19 @@
+
+public class ParamertizedConstructor {
+
+	
+	public static void main(String[] args) {
+
+		Employee e1 = new Employee(101, "ajeet", 45000);
+
+		Employee e2 = new Employee(102, "irfan", 25000);
+
+		Employee e3 = new Employee(103, "nakul", 55000);
+
+		e1.display();
+
+		e2.display();
+
+		e3.display();
+}
+}
